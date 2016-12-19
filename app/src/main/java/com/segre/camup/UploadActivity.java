6c9c130341cf4fc5a6193d6e3f19ee2293775e27
Oscar Lérida Monsó <olerida@icloud.com>
2016-12-19 22:48:22 +0100

    llista de uploads amb ok

--- app/src/main/java/com/segre/camup/UploadActivity.java
+++ app/src/main/java/com/segre/camup/UploadActivity.java
@@ -1,16 +1,29 @@
 package com.segre.camup;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Environment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
@@ -24,7 +37,7 @@ import java.io.InputStream;
 public class UploadActivity extends AppCompatActivity {
 
     static final String PATH = "file:///storage/emulated/0/DCIM";
-    static final String HOST = "ftp.segre.com";
+    static final String HOST = "10.8.6.125";
     static final String USER = "foto";
     static final String PWD = "f.oto";
     static final String ALBUM = "proves_oscar";
@@ -35,6 +48,15 @@ public class UploadActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
+
+/*        ProgressBar progressBar = new ProgressBar(this);
+        progressBar.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        // Must add the progress bar to the root of the layout
+        ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
+        root.addView(progressBar);*/
+
         start();
     }
 
@@ -44,7 +66,7 @@ public class UploadActivity extends AppCompatActivity {
         try {
             ftp.logout();
             ftp.disconnect();
-            System.out.println("FTP connection closed.");
+            Log.d("FTP", "FTP connection closed.");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -100,7 +122,6 @@ public class UploadActivity extends AppCompatActivity {
                 }
 
 
-
                 // Check and go to working directory (album)
                 try {
                     new ftpChangeDirTask().execute(ALBUM);
@@ -108,12 +129,41 @@ public class UploadActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
 
+                LinearLayout vLayout = (LinearLayout) findViewById(R.id.vLayout);
+
+
                 for (int i = 0; i < files.length; i++)
                 {
                     Log.d("Files", "FileName:" + files[i].getName());
+
+                    LinearLayout hLayout = new LinearLayout( this );
+                    hLayout.setOrientation( LinearLayout.HORIZONTAL );
+                    vLayout.addView( hLayout );
+
+                    RelativeLayout rLayout = new RelativeLayout(this);
+
+                    ImageView img = new ImageView( this );
+                    Drawable d = Drawable.createFromPath( files[i].getAbsolutePath() );
+                    img.setImageDrawable(d);
+                    ViewGroup.LayoutParams lp =  new ViewGroup.LayoutParams(300,300);
+                    img.setLayoutParams(lp);
+                    rLayout.addView(img); // 0
+                    rLayout.addView( new ProgressBar( this ) ); // 1
+                    Drawable dok =  ContextCompat.getDrawable(this, R.drawable.ok);
+                    ImageView imgok = new ImageView(this);
+                    imgok.setImageDrawable(dok);
+                    imgok.setVisibility(View.INVISIBLE);
+                    rLayout.addView(imgok); // 2
+
+                    hLayout.addView(rLayout);
+
+                    TextView tv = new TextView(this);
+                    tv.setText(files[i].getName());
+                    hLayout.addView(tv);
+
                     // Call Upload files task
                     try {
-                        Boolean uploaded = new ftpUploadFilesTask().execute(files[i]).get();
+                        Boolean uploaded = new ftpUploadFilesTask(this, hLayout).execute(files[i]).get();
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
@@ -144,11 +194,21 @@ public class UploadActivity extends AppCompatActivity {
     }
 
     private class ftpUploadFilesTask extends AsyncTask<File, Void, Boolean> {
+        File file;
+        Context mc;
+        LinearLayout ll;
+
+        public ftpUploadFilesTask(Context c, LinearLayout l) {
+            mc = c; ll = l;
+        }
+
         @Override
         protected Boolean doInBackground(File... files) {
+
+
             Log.d("Files", "Uploading: " + files[0].getName());
             InputStream input;
-            File file = files[0];
+            file = files[0];
 
             // Create stream and upload to ftp
             try {
@@ -158,10 +218,27 @@ public class UploadActivity extends AppCompatActivity {
                 input.close();
             } catch (FileNotFoundException e) {
                 Log.w("FTP", "File not found: " + files[0].getName());
+                return false;
             } catch (IOException e) {
                 Log.e("FTP", "Error on upload file: " + file.getName());
+                return false;
             }
             return true;
+        }
+
+        protected void onPostExecute(Boolean uploaded) {
+            if (uploaded) {
+                Toast.makeText(mc, file.getName() + " Uploaded.", Toast.LENGTH_LONG).show();
+                RelativeLayout rl = (RelativeLayout) ll.getChildAt(0);
+                ProgressBar pb = (ProgressBar) rl.getChildAt(1);
+                pb.setVisibility(View.INVISIBLE);
+                ImageView imgok = (ImageView) rl.getChildAt(2);
+                imgok.setVisibility(View.VISIBLE);
+
+            } else {
+                Toast.makeText(mc, file.getName() + " cant upload.", Toast.LENGTH_LONG).show();
+            }
+
         }
     }
 
