ef3f3e787a73ba08903da097364a56a1c2ec31ba
Oscar Lérida Monsó <olerida@icloud.com>
2016-12-21 22:53:35 +0100

    beta 1, amb preferencies

--- app/src/main/java/com/segre/camup/SelectActivity.java
+++ app/src/main/java/com/segre/camup/SelectActivity.java
@@ -1,9 +1,17 @@
 package com.segre.camup;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import static android.provider.LiveFolders.INTENT;
 
@@ -15,10 +23,47 @@ public class SelectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select);
+
+        Log.i("PREF",
+                PreferenceManager
+                .getDefaultSharedPreferences(this)
+                .getString("ftp_server", "")
+        );
+
     }
 
     public void upClick(View view) {
         Intent intent = new Intent(this, UploadActivity.class);
         startActivity(intent);
+    }
+
+    public void cfgClick(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Contrassenya de administrador");
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        //input.setTransformationMethod(PasswordTransformationMethod.getInstance());
+        builder.setView(input);
+        final Context c = this;
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (input.getText().toString().equals("prem.goras")) {
+                    Intent intent = new Intent(c, SettingsActivity.class);
+                    startActivity(intent);
+                }
+                dialog.cancel();
+            }
+        });
+
+
+        builder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+
+            }
+        });
+        builder.show();
     }
 }
--- app/src/main/java/com/segre/camup/UploadActivity.java
+++ app/src/main/java/com/segre/camup/UploadActivity.java
@@ -12,14 +12,17 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Environment;
+import android.preference.PreferenceManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutCompat;
+import android.text.InputType;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -37,15 +40,16 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.concurrent.ExecutionException;
 
 public class UploadActivity extends AppCompatActivity {
 
     //static final String PATH = "file:///storage/emulated/0/DCIM/Wi-Fi SD";
-    static final String PATH = "Wi-Fi SD";
-    static final String HOST = "ftp.segre.com";
-    static final String USER = "foto";
-    static final String PWD = "f.oto";
-    static final String ALBUM = "proves_oscar";
+    static String PATH;
+    static String HOST;
+    static String USER;
+    static String PWD;
+    String album = "default";
 
     FTPClient ftp = new FTPClient();
 
@@ -53,6 +57,12 @@ public class UploadActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
+
+        //Log.i("PREF", "" + getSharedPreferences(PREF_NAME, MODE_PRIVATE).getString());
+        PATH = PreferenceManager.getDefaultSharedPreferences(this).getString("local_path", "");
+        HOST = PreferenceManager.getDefaultSharedPreferences(this).getString("ftp_server", "");
+        USER = PreferenceManager.getDefaultSharedPreferences(this).getString("ftp_user", "");
+        PWD = PreferenceManager.getDefaultSharedPreferences(this).getString("ftp_pwd", "");
     }
 
     @Override
@@ -97,121 +107,181 @@ public class UploadActivity extends AppCompatActivity {
                 connected = new ftpConnectTask().execute(HOST, USER, PWD).get();
             } catch (Exception e) {
                 e.printStackTrace();
+                Log.e("FTP", "Error on ftp connect.");
             }
 
+            if (!connected) {
+                AlertDialog alertDialog = new AlertDialog.Builder(UploadActivity.this).create();
+                alertDialog.setTitle("FTP connection");
+                alertDialog.setMessage("No s'ha pogut fer la connexió amb el servidor.\n" + ftp.getReplyString());
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                System.exit(0);
+                            }
+                        });
+                alertDialog.show();
+            }
+
+            File[] files = null;
+            File imagesDir = null;
             // check storage state
             String state = Environment.getExternalStorageState();
             if (Environment.MEDIA_MOUNTED.equals(state) ||
                     Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
-                File imagesDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).getAbsolutePath(), PATH);
+                imagesDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).getAbsolutePath(), PATH);
                 //File imagesDir = new File("/sdcard/DCIM/Camera");
 
                 Log.d("Files", "Path: " + imagesDir.getPath());
                 Log.d("Files", "Exist: " + imagesDir.exists());
-                Log.d("Files", "IsDir: " + imagesDir.exists());
+                Log.d("Files", "IsDir: " + imagesDir.isDirectory());
                 Log.d("Files", "CanRead: " + imagesDir.canRead());
-                final File[] files = imagesDir.listFiles();
-                Log.d("Files", "Images on dir: "+ files.length);
+                files = imagesDir.listFiles();
+                Log.d("Files", "Images on dir: " + (files != null ? files.length : "null"));
 
                 if (!imagesDir.exists() || !imagesDir.canRead()) {
                     Log.d("FILES", "Cant access filesystem");
-                    return;
+                    alert("Fotos", "No tinc acces a la carpeta " + PATH);
+                    //System.exit(0);
                 }
 
-                // Check and go to working directory (album)
-                try {
-                    new ftpChangeDirTask().execute(ALBUM);
-                } catch (Exception e) {
-                    e.printStackTrace();
+                if (files == null || files.length == 0) {
+                    alert("Fotos", "No hi ha fotos a la carpeta " + PATH);
+                    Log.d("FILES", "No files to upload");
+                    //System.exit(0);
                 }
+            }
 
+            if (imagesDir != null && imagesDir.canRead()  && imagesDir.exists() && imagesDir.isDirectory() && files != null && files.length > 0) {
+                // Input album name
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                //final String[] inputText = new String[1];
+                builder.setTitle("Nom del Album");
+
+                // Set up the input
+                final EditText input = new EditText(this);
+                // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+                input.setInputType(InputType.TYPE_CLASS_TEXT);
+                builder.setView(input);
+
+                // Set up the buttons
+                final File[] finalFiles = files;
                 final Context c = this;
-                final LinearLayout vl = (LinearLayout) findViewById(R.id.vLayout);
-                    new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            for ( int i = 0; i < files.length; i++) {
-                                final File f =  files[i];
-                                Log.d("Files", "FileName:" + files[i].getName());
-
-                                runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        RelativeLayout rl = new RelativeLayout(c);
-                                        ImageView img = new ImageView(c);
-                                        BitmapFactory.Options bopt = new BitmapFactory.Options();
-                                        bopt.inSampleSize = 2;
-                                        Bitmap b = BitmapFactory.decodeFile(f.getAbsolutePath(), bopt);
-                                        Drawable d = new BitmapDrawable(getResources(), b);
-                                        img.setImageDrawable(d);
-                                        img.setLayoutParams(new LinearLayoutCompat.LayoutParams(300, 300));
-                                        img.setForegroundGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
-                                        rl.addView(img); // 0 imatge de la foto
-                                        ProgressBar pg = new ProgressBar(c);
-                                        pg.setForegroundGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
-                                        rl.addView(pg); // 1 progressbar
-                                        b = BitmapFactory.decodeResource(getResources(), R.drawable.completed, bopt);
-                                        img = new ImageView(c);
-                                        img.setLayoutParams(new LinearLayoutCompat.LayoutParams(100, 100));
-                                        d = new BitmapDrawable(getResources(), b);
-                                        img.setImageDrawable(d);
-                                        img.setVisibility(View.INVISIBLE);
-                                        rl.addView(img); // 2 imatge del ok
-                                        b = BitmapFactory.decodeResource(getResources(), R.drawable.error, bopt);
-                                        img = new ImageView(c);
-                                        img.setLayoutParams(new LinearLayoutCompat.LayoutParams(100, 100));
-                                        d = new BitmapDrawable(getResources(), b);
-                                        img.setImageDrawable(d);
-                                        img.setVisibility(View.INVISIBLE);
-                                        rl.addView(img); // 3 imatge del error
-
-                                        LinearLayout hl = new LinearLayout(c); hl.setOrientation(LinearLayout.HORIZONTAL);
-                                        hl.addView(rl);
-                                        TextView tv = new TextView(c);
-                                        tv.setText(f.getName());
-                                        hl.addView(tv);
-
-                                        vl.addView(hl);
-                                        ScrollView sc = (ScrollView) findViewById(R.id.scrView);
-                                        sc.fullScroll(View.FOCUS_DOWN);
-                                    }
-                                });
-
-                                // Call Upload files task
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        album = input.getText().toString();
+                        final LinearLayout vl = (LinearLayout) findViewById(R.id.vLayout);
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                // Check and go to working directory (album)
+                                Boolean ftpdir = false;
                                 try {
-                                    Boolean uploaded = new ftpUploadFilesTask(c, i).execute(f).get();
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-                                }
+                                    ftpdir = new ftpChangeDirTask().execute(album).get();
+                                } catch (Exception e) {}
+                                for (int i = 0; i < finalFiles.length; i++) {
+                                    final File f =  finalFiles[i];
+                                    Log.d("Files", "FileName:" + finalFiles[i].getName());
 
+                                    runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            RelativeLayout rl = new RelativeLayout(c);
+                                            ImageView img = new ImageView(c);
+                                            BitmapFactory.Options bopt = new BitmapFactory.Options();
+                                            bopt.inSampleSize = 2;
+                                            Bitmap b = BitmapFactory.decodeFile(f.getAbsolutePath(), bopt);
+                                            Drawable d = new BitmapDrawable(getResources(), b);
+                                            img.setImageDrawable(d);
+                                            img.setLayoutParams(new LinearLayoutCompat.LayoutParams(300, 300));
+                                            img.setForegroundGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
+                                            rl.addView(img); // 0 imatge de la foto
+                                            ProgressBar pg = new ProgressBar(c);
+                                            pg.setForegroundGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
+                                            rl.addView(pg); // 1 progressbar
+                                            b = BitmapFactory.decodeResource(getResources(), R.drawable.completed, bopt);
+                                            img = new ImageView(c);
+                                            img.setLayoutParams(new LinearLayoutCompat.LayoutParams(100, 100));
+                                            d = new BitmapDrawable(getResources(), b);
+                                            img.setImageDrawable(d);
+                                            img.setVisibility(View.INVISIBLE);
+                                            rl.addView(img); // 2 imatge del ok
+                                            b = BitmapFactory.decodeResource(getResources(), R.drawable.error, bopt);
+                                            img = new ImageView(c);
+                                            img.setLayoutParams(new LinearLayoutCompat.LayoutParams(100, 100));
+                                            d = new BitmapDrawable(getResources(), b);
+                                            img.setImageDrawable(d);
+                                            img.setVisibility(View.INVISIBLE);
+                                            rl.addView(img); // 3 imatge del error
+
+                                            LinearLayout hl = new LinearLayout(c); hl.setOrientation(LinearLayout.HORIZONTAL);
+                                            hl.addView(rl);
+                                            TextView tv = new TextView(c);
+                                            tv.setText(f.getName());
+                                            hl.addView(tv);
+
+                                            vl.addView(hl);
+                                            ScrollView sc = (ScrollView) findViewById(R.id.scrView);
+                                            sc.fullScroll(View.FOCUS_DOWN);
+                                        }
+                                    });
+
+                                    // Call Upload files task
+                                    try {
+                                        if (ftpdir) {
+                                            Boolean uploaded = new ftpUploadFilesTask(c, i).execute(f).get();
+                                        } else {
+                                            Toast.makeText(c, "Album incorrecte", Toast.LENGTH_SHORT).show();
+                                            System.exit(0);
+                                        }
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+
+                                }
                             }
-                        }
-                    }).start();
+                        }).start();
+
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                        System.exit(0);
+                    }
+                });
+                builder.show();
             }
 
         } else {
             alert("Sin conexión", "Comprueba que tienes conexion WIFI o de datos moviles");
-
         }
     }
 
-    private class ftpChangeDirTask extends AsyncTask<String, Void, Void> {
+
+    // FTP Change dir task
+    private class ftpChangeDirTask extends AsyncTask<String, Void, Boolean> {
         @Override
-        protected Void doInBackground(String... dirs) {
+        protected Boolean doInBackground(String... dirs) {
             try {
                 String dir = dirs[0];
                 if (!ftp.changeWorkingDirectory(dir)) {
                     ftp.makeDirectory(dir);
-                    ftp.changeWorkingDirectory(dir);
+                    return ftp.changeWorkingDirectory(dir);
                 }
                 Log.d("FTP", ftp.getReplyString());
+                return false;
             } catch (IOException e) {
                 e.printStackTrace();
+                return false;
             }
-            return null;
         }
     }
 
+    // FTP Upload FILE
     private class ftpUploadFilesTask extends AsyncTask<File, Void, Boolean> {
         File file;
         Context mc;
@@ -274,7 +344,7 @@ public class UploadActivity extends AppCompatActivity {
         }
     }
 
-    // Connect and login on FTP servere task
+    // ftp - Connect and login on FTP server task
     private class ftpConnectTask extends AsyncTask<String, Void, Boolean> {
         String host, user, pwd;
 
